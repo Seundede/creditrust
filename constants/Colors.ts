cc96b6f2294e90557be15ca75d0f67723399f474
Seundede <omadoyedede0@gmail.com>
2024-06-01 12:52:30 +0100

    chore: code clean up

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,7 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  primary: "#083E9E",
+  primary50: "#6184C1",
+  secondary: "#111928",
+  gray: "#9DA3AF",
+  grey50: "#E5E7EB",
 };

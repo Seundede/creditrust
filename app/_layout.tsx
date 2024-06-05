355c23c256c7c7eab3541081d1312934a34224ba
Seundede <omadoyedede0@gmail.com>
2024-06-05 21:23:10 +0100

    chore: auth layout page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
-
+import * as SecureStore from "expo-secure-store";
 
 
 export {
@@ -45,13 +49,88 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
+const router = useRouter()
+//Function to encrypt and securely store key-value pairs locally on the device.
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
   return (
-
-      <Stack>
-       
-      </Stack>
-
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="signup"
+            options={{
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons name="arrow-back" size={24} />
+                </TouchableOpacity>
+              ),
+              title: "",
+              headerBackTitle: "",
+              headerShadowVisible: true,
+            }}
+          />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerRight: () => (
+                <Link href="help" asChild>
+                  <TouchableOpacity>
+                    <Ionicons name="help-circle-outline" size={24} />
+                  </TouchableOpacity>
+                </Link>
+              ),
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons name="arrow-back" size={24} />
+                </TouchableOpacity>
+              ),
+              title: "",
+              headerBackTitle: "",
+              headerShadowVisible: true,
+            }}
+          />
+          <Stack.Screen
+            name="verify/[phone]"
+            options={{
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons name="arrow-back" size={24} />
+                </TouchableOpacity>
+              ),
+              title: "",
+              headerBackTitle: "",
+              headerShadowVisible: true,
+            }}
+          />
+          <Stack.Screen
+            name="help"
+            options={{
+              title: "Help",
+              presentation: "modal",
+            }}
+          />
+        </Stack>
+      </GestureHandlerRootView>
+    </ClerkProvider>
   );
 }
